d9552580d84751cdd950f7820d3b7c15e99824f9
Gustavo Victor <gguvictor909@gmail.com>
2024-06-24 18:48:04 -0300

    feat: add search movies functionality

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,37 @@
+import { useState, FormEvent } from "react";
+import { useNavigate } from "react-router-dom"; 
 import { NavLink } from "react-router-dom"; 
 import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
 import "./style.css"; 
 
 
 export default function Header() {
+  const [search, setSearch] = useState(""); 
+  const navigate = useNavigate(); 
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault(); 
+
+    if(search) {      
+      navigate(`/search?q=${search}`); 
+      setSearch(""); 
+    } else {
+      return ;
+    }
+  }
+
   return (
     <div id="header">
       <nav id="navbar">
         <h2 id="logo"><NavLink to="/"><BiCameraMovie/> MoviesLib</NavLink></h2>
-        <form id="search-form" action="#">
+        <form onSubmit={handleSubmit} id="search-form" action="#">
           <input 
             type="search" 
             name="search" 
             id="search" 
             placeholder="Search for a movie..."
+            value={search || ""}
+            onChange={(e) => setSearch(e.target.value)}
              />
           <button className="btn" type="submit">
             <BiSearchAlt2 />
--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,10 +1,56 @@
-import "./style.css"; 
+// import { useFetchData } from "../../hooks/useFetchData"
+import { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import MovieCard from "../../components/MovieCard";
+import { IMovie } from "../../interfaces/Movie";
+import "./style.css";
+const moviesSearchURL = String(import.meta.env.VITE_SEARCH);
+//const apiKey = String(import.meta.env.VITE_API_KEY);
+const token = String(import.meta.env.VITE_API_TOKEN);
 
 
 export default function Search() {
+  const [movies, setMovies] = useState<IMovie[]>([])
+  const [search] = useSearchParams();
+  const [loading, setLoading] = useState(true);
+  const searchText = search.get("q");
+  // const customUrl = `${searchUrl}?api_key=${apiKey}&query=${searchText}`
+  // const { data: movies, loading } = useFetchData(customUrl); 
+
+  const getMoviesBySearch = async (url: string) => {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${token}`
+      }
+    });
+
+    const data = await response.json();
+    setMovies(data.results);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    const customUrl = `${moviesSearchURL}?query=${searchText}`;
+    getMoviesBySearch(customUrl);
+  }, [searchText]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
   return (
-    <div>
-      <h1>Search</h1>
+    <div className="container">
+      <h1 className="title">Results for: <span className="search-text">{searchText}</span></h1>
+      <div className="movies-container">
+        {!loading && movies && movies.length > 0 &&
+          movies.map(movie => (
+            <MovieCard key={movie.id} movie={movie} showLink={true} />
+          ))
+        }
+        {!loading && movies && movies.length == 0 && <p>There are no movies</p>}
+      </div>
     </div>
   )
 }
